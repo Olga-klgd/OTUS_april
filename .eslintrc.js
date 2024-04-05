ad84790bc3af8ba4344954dd6f08056d6e7a074c
Olga <olga-klgd@yandex.ru>
2024-04-05 14:34:58 +0200

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "jest/globals": true
     },
     "extends": "eslint:recommended",
     "overrides": [
@@ -21,6 +22,8 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "ignorePatterns": [".eslintrc.js", "babel.config.js", "jest.config.js"],
+    "plugins": ["jest"],
     "rules": {
     }
 }
