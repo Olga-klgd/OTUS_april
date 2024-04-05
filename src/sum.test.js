1ec72dd17a96af9a1d834ae7b1aacc11823af96d
Olga <olga-klgd@yandex.ru>
2024-04-05 15:11:25 +0200

    add prettierr

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,6 +1,6 @@
 import { sum } from "./sum";
-describe ('sum', () => {
-    it('return sum', () => {
-        expect(sum(1, 2)).toEqual(3);    
-    })
-})
+describe("sum", () => {
+  it("return sum", () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+});
